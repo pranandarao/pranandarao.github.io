e02ee028488580611ea52ca0c7e5ca9123133bd1
pranandarao <pranav.anandarao@gmail.com>
2020-03-25 16:43:22 -0500

    added header and background gif, added typography plugin and changed theme, need to make a background without an image and fix text background opacity

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,19 @@
 import React from "react"
-import { Link } from 'gatsby'
+//import { Link } from 'gatsby'
 
+/*
 const ListLink = props => (
     <li style={{ display: `inline-block`, marginRight: `1rem` }}>
       <Link to={props.to}>{props.children}</Link>
     </li>
-  )
+)
+*/
 
 export default ({ children }) => (
-    <div style={{ margin: `3rem auto`, maxWidth: 550, padding: `0 1rem` }}>
+    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem`, backgroundColor: 'gray'}}>
+        <header>
+            <h1>Welcome to my website!</h1>
+        </header>
         {children}
     </div>
 )
---
+++ src/utils/typography.js
@@ -0,0 +1,11 @@
+import Typography from "typography"
+import grandviewTheme from "typography-theme-grand-view"
+
+grandviewTheme.bodyColor = 'white';
+grandviewTheme.headerColor = 'white';
+
+const typography = new Typography(
+    grandviewTheme
+);
+
+export default typography;  
